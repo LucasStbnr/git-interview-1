f70376492ef1b7c9e362ddff86dc775d92f800cc
Yanis Berger <yanis.berger@example.com>
2024-03-06 10:06:00 +0100

    feat(cli): ajoute la commande list

--- src/cli.js
+++ src/cli.js
@@ -6,4 +6,8 @@ const command = process.argv[2] || 'total';
 
 if (command === 'total') {
   console.log('Total TTC : ' + total(DEMO_CART).toFixed(2) + ' EUR');
+} else if (command === 'list') {
+  DEMO_CART.forEach(function (item) {
+    console.log(item.sku + ' x' + item.quantity + ' - ' + item.label);
+  });
 }
